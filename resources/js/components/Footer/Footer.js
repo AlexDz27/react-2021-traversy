e118c105949e25655f78dc0810f8c6084d6dad1b
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-02-04 19:43:52 +0300

    Fix: fix text in restore tasks button

--- resources/js/components/Footer/Footer.js
+++ resources/js/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import Button from "../Button/Button";
 const Footer = ({onRestore}) => {
   return (
     <footer>
-      <Button onClick={() => onRestore()} text="Restore default buttons" color="steelblue" />
+      <Button onClick={() => onRestore()} text="Restore default tasks" color="steelblue" />
       <p className="text-muted" style={{marginTop: 10}}>
         Followed by&nbsp;
         <a className="text-muted" href="https://www.youtube.com/watch?v=w7ejDZ8SWv8">Traversy Media video</a> | 2021
